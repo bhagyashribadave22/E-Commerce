ffe83e73d3f9c10405d3ce0c22b68ede49c4396c
unknown <bhagyashribadave4@gmail.com>
2025-06-25 19:37:27 +0530

    mens, womens, kids component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Shop from './Pages/Shop';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from "./assets/banner.jpg";
+import women_banner from "./assets/women_banner.avif";
+import kids_banner from "./assets/kids_banner.jpg";
 
 function App() {
   return (
@@ -14,9 +18,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>}/>
-        <Route path="/mens" element={<ShopCategory category="mens"/>} />
-        <Route path="/womens" element={<ShopCategory category="womens"/>}/>
-        <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>}/>
 
         <Route path='/product' element={<Product/>}/>
         <Route path=':productId' element={<Product/>}/>
@@ -24,6 +28,8 @@ function App() {
         <Route path='/login' element={<LoginSignup/>}/>
 
       </Routes>
+      <Footer/>
+
       </BrowserRouter>
     </div>
   );
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,27 +2,28 @@ import React from 'react';
 import './Hero.css';
 import hand_icon from '../../assets/hand_icon.png';
 import arrow_icon from '../../assets/arrow_icon.png';
-import men from '../../assets/men.webp';
+import men from '../../assets/p8.webp';
 
 const Hero = () => {
     return (
         <div className="hero">
             <div className="hero-left">
-                <h2>Best deals! Best Prices</h2>
+                <h2>Best Deals ! Best Prices !</h2>
 
-                <div className="hand-hand-icon">
+                <div className="hero-hand-icon">
                     <p>new</p>
                     <img src={hand_icon} alt="" style={{ height: "60px" }} />
                 </div>
                 <p>Collections</p>
                 <p>for everyone</p>
-            </div>
-            <div className="hero-latest-btn">
+                 <div className="hero-latest-btn">
                 <div>Latest Collections</div>
                 <img src={arrow_icon} alt="" height="30px"/>
             </div>
+            </div>
+           
             <div className="hero-right">
-                <img src={men} alt="" />
+                <img src={men} alt="" height="400px"/>
             </div>
         </div>
     );
